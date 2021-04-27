a50438225eace7cc59a1a30c29d52c1e69e62f8b
Simon Berz <simon@berz.one>
2021-04-27 16:49:30 +0200

    feat: add prepare-msg-flag action

---
+++ prepare-img-tag/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/snapADDY/actions/pkg/actions"
+	"github.com/snapADDY/actions/pkg/msgflag"
+)
+
+func main() {
+	ctx := actions.NewContext()
+	fmt.Printf("Actions Context: %#v\n", ctx)
+
+	commitMsg := os.Getenv("INPUT_COMMITMESSAGE")
+	flags := msgflag.Parse(commitMsg)
+
+	fmt.Printf("Parsed commit message: %s\nFlags: %#v\n", commitMsg, flags)
+
+	tag, publish := makeImgTag(ctx, flags)
+
+	fmt.Printf("Prepared Tag: %s; Should publish %v\n", tag, publish)
+
+	actions.SetOutput("tag-name", tag)
+	actions.SetOutput("publish", publish)
+
+}
+
+var alwaysPublishBranch = map[string]bool{
+	"master":  true,
+	"main":    true,
+	"staging": true,
+}
+
+func makeImgTag(ctx actions.Context, flags msgflag.Flags) (string, bool) {
+	tagRegex := regexp.MustCompile(`^\w[\w.-]{0,127}$`)
+	invalidChar := regexp.MustCompile(`[^\w.-]+`)
+
+	if ctx.EventName == "push" {
+		if ctx.Ref.Type == actions.Tag && tagRegex.MatchString(ctx.Ref.Name) {
+			return ctx.Ref.Name, true
+
+		} else if ctx.Ref.Type == actions.Branch {
+			br := ctx.Ref.Name
+			br = strings.ReplaceAll(br, "/", "")
+			br = invalidChar.ReplaceAllString(br, "")
+
+			_, pub := alwaysPublishBranch[br]
+			pub = pub || flags.Img.ShouldPublish
+
+			ov := flags.Img.EnvNameOverride
+			if ov != "" && tagRegex.MatchString(ov) {
+				br = ov
+			}
+
+			hash := ctx.SHA[:8]
+			tag := fmt.Sprintf("%s-%s-%d", br, hash, ctx.RunNumber)
+
+			return tag, pub
+		}
+	}
+
+	return fmt.Sprintf("run-%d", ctx.RunNumber), false
+}
