fb763d9e469b55fcbde2dcb3bacdcbe6110d21d2
Simon Berz <simon@berz.me>
2022-02-28 19:30:54 +0100

    build(release): v1.1.0

---
+++ release.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "No version supplied"
+  exit 1
+fi
+
+version=$1
+
+if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  >&2 echo "Invalid Version: $version"
+  exit 1
+fi
+
+
+for action in `find . -type f -name action.yaml`; do
+  action_name=$(basename $(dirname $action))
+  sed -i -E -e "s/${action_name}:[0-9]+\.[0-9]+\.[0-9]+/${action_name}:${version}/g" $action
+  git add $action
+done
+
+git commit -m "build(release): v${version}"
+git tag -m "v${version}" v${version}
