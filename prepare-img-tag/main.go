ed6336cb33816a8179badb404d03f0db1cbce0fe
Simon Berz <simon@berz.me>
2022-07-12 12:52:05 +0200

    feat(prepare-img-tag): add flag outputs

--- prepare-img-tag/main.go
+++ prepare-img-tag/main.go
@@ -37,25 +37,29 @@ func run() error {
 	log.Printf("Parsed commit message: %s\nFlags: %#v\n", commitMsg, flags)
 
 	imgFlag := parseImgFlag(flags["img"])
-	tag, publish := makeImgTag(action, imgFlag)
+	tag, envName, publish := makeImgTag(action, imgFlag)
 
 	log.Printf("Prepared Tag: %s; Should publish %v\n", tag, publish)
 
 	actions.SetOutput("tag-name", tag)
 	actions.SetOutput("publish", publish)
 
+	actions.SetOutput("has-img-flag", imgFlag.ShouldPublish)
+	actions.SetOutput("env-name", envName)
+	actions.SetOutput("env-override", imgFlag.EnvNameOverride)
+
 	return nil
 }
 
 // makeImgTag creates the tag for our container registry and decides if the image should be published
-func makeImgTag(action actions.Context, imgFlag ImgFlag) (string, bool) {
+func makeImgTag(action actions.Context, imgFlag ImgFlag) (tag, envName string, publish bool) {
 	tagRegex := regexp.MustCompile(`^\w[\w.-]{0,127}$`)
 	invalidChar := regexp.MustCompile(`[^\w.-]+`)
 
 	if action.EventName == "push" {
 		if action.Ref.Type == actions.Tag && tagRegex.MatchString(action.Ref.Name) {
 			// Tags should always be published.
-			return action.Ref.Name, true
+			return action.Ref.Name, action.Ref.Name, true
 		} else if action.Ref.Type == actions.Branch {
 			// Clean the branch name to be compatible with container registry tags.
 			br := action.Ref.Name
@@ -78,10 +82,10 @@ func makeImgTag(action actions.Context, imgFlag ImgFlag) (string, bool) {
 			hash := action.SHA[:8]
 			tag := fmt.Sprintf("%s-%s-%d", br, hash, action.RunNumber)
 
-			return tag, pub
+			return tag, br, pub
 		}
 	}
 
 	// We don't want to publish but still need a placeholder tag for CI.
-	return fmt.Sprintf("run-%d", action.RunNumber), false
+	return fmt.Sprintf("run-%d", action.RunNumber), "", false
 }
