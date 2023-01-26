c3a987b4a9e7aa4240dc1e281d3a0ba7aa52cf86
Simon Berz <simon@berz.me>
2023-01-26 14:22:49 +0100

    fix(go): reimplement SetOutput with output files

--- internal/actions/actions.go
+++ internal/actions/actions.go
@@ -1,7 +1,36 @@
 package actions
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func SetOutput(name string, value interface{}) {
-	fmt.Printf("::set-output name=%s::%v\n", name, value)
+const (
+	fileCMDOutput = "GITHUB_OUTPUT"
+)
+
+func SetOutput(key string, value any) {
+	err := fileCMD(fileCMDOutput, fmt.Sprintf("%s=%v", key, value))
+	if err != nil {
+		// We can't recover from this state
+		panic(err)
+	}
+}
+
+func fileCMD(cmd, msg string) error {
+	filepath := os.Getenv(cmd)
+
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return fmt.Errorf("can not open environment file: %w", err)
+	}
+
+	defer f.Close()
+
+	_, err = fmt.Fprintln(f, msg)
+	if err != nil {
+		return fmt.Errorf("can not write to environment file: %w", err)
+	}
+
+	return nil
 }
